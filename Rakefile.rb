d68a748197915920ec0c46b206bb3295f7dcf9cf
Ryan Grove <ryan@wonko.com>
2008-04-16 16:36:37 -0700

    update gem dependencies

--- Rakefile.rb
+++ Rakefile.rb
@@ -62,18 +62,17 @@ thoth_gemspec = Gem::Specification.new do |s|
   s.required_ruby_version = '>= 1.8.6'
 
   s.add_dependency('ramaze',        '~> 0.3.9.5')
-  s.add_dependency('builder',       '>= 2.1.2')
-  s.add_dependency('configuration', '>= 0.0.5')
+  s.add_dependency('builder',       '~> 2.1.2')
+  s.add_dependency('configuration', '~> 0.0.5')
   s.add_dependency('cssmin',        '~> 1.0.0')
   s.add_dependency('erubis',        '>= 2.5.0')
-  s.add_dependency('hpricot',       '>= 0.6')
+  s.add_dependency('hpricot',       '~> 0.6')
   s.add_dependency('json_pure',     '>= 1.1.2')
   s.add_dependency('jsmin',         '~> 1.0.0')
   s.add_dependency('RedCloth',      '>= 3.0.4')
-  s.add_dependency('sequel',        '>= 1.3')
-  s.add_dependency('sequel_core',   '>= 1.3')
-  s.add_dependency('sequel_model',  '>= 0.5.0.2')
-  s.add_dependency('thin',          '>= 0.7.1')
+  s.add_dependency('sequel',        '~> 1.4.0')
+  s.add_dependency('sequel_core',   '~> 1.4.0')
+  s.add_dependency('thin',          '~> 0.8.0')
 end
 
 plugins = []
