b8e379ea32bdca3b6f4d870d935e61cdabee7f65
Ryan Grove <ryan@wonko.com>
2009-02-16 21:26:31 -0800

    Bump Sanitize dependency to 1.0.5.

--- Rakefile.rb
+++ Rakefile.rb
@@ -69,7 +69,7 @@ thoth_gemspec = Gem::Specification.new do |s|
   s.add_dependency('json_pure',         '~> 1.1.3')
   s.add_dependency('jsmin',             '~> 1.0.1')
   s.add_dependency('RedCloth',          '~> 4.1.1')
-  s.add_dependency('sanitize',          '~> 1.0.4')
+  s.add_dependency('sanitize',          '~> 1.0.5')
   s.add_dependency('sequel',            '~> 2.9.0')
   s.add_dependency('sequel_notnaughty', '~> 0.6.2')
   s.add_dependency('thin',              '~> 1.0.0')
